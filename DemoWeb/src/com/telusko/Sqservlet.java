1329fc78877585422c088fd3dca5661010499f74
JyotiRSharma <appdeveloperubuntu@gmail.com>
2020-12-01 17:15:23 +0530

    Demo with servlet and JSP

---
+++ DemoWeb/src/com/telusko/Sqservlet.java
@@ -0,0 +1,21 @@
+package com.telusko;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Sqservlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = (int) req.getAttribute("k");
+		
+		k *= k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Square of k is " + k);
+	}
+}
